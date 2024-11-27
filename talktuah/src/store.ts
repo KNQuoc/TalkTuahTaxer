291bfeb6c2479115be2f45323aeaf43f5a649cec
Quoc Kien Ngo <156245273+KNQuoc@users.noreply.github.com>
2024-11-27 17:18:40 -0500

    deleted, redo

--- talktuah/src/store.ts
+++
@@ -1,9 +0,0 @@
-// src/store.ts
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
