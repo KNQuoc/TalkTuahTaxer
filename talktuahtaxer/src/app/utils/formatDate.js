233fd2ad7f001f924657fd95b8b81bd5c9c94151
Thi Nguyen <168005804+tamthah@users.noreply.github.com>
2024-11-27 23:30:08 -0600

    done with manifest and some function

---
+++ talktuahtaxer/src/app/utils/formatDate.js
@@ -0,0 +1,8 @@
+// src/utils/formatDate.js
+export function formatDate(date) {
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }).format(new Date(date));
+  }
