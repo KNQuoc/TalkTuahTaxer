d71e3b868ba6947ba725a4014868026c388cac9b
Quoc Kien Ngo <156245273+KNQuoc@users.noreply.github.com>
2024-11-28 00:57:56 -0500

    BASICS DONE I THINK, NO ERRORS!

--- talktuahtaxer/src/app/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-// src/utils/formatDate.js
-export function formatDate(date) {
-    return new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    }).format(new Date(date));
-  }
